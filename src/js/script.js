93b43830ba7bf6e8e0eec0eaa690f7dede5c1bdd
Oseias Nogueira <oseias.n.m@gmail.com>
2023-03-08 17:25:45 -0300

    Mapeamento do camimnho dos áudio de notificações com o JS

--- src/js/script.js
+++ src/js/script.js
@@ -3,3 +3,7 @@ let pausa = document.getElementById('pausa')
 let sessoes = document.getElementById('sessoes')
 let segundos
 
+var retorno = new Audio("./src/audio/retorno.mp3")
+var fim = new Audio("./src/audio/fim.mp3")
+
+
