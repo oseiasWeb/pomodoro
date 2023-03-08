60fee1f2aaa560279a42282f72ebb7771dd25f0b
Oseias Nogueira <oseias.n.m@gmail.com>
2023-03-08 18:06:42 -0300

    Validando se o usuário está preenchendo os campos obrigatórios via JS

--- src/js/script.js
+++ src/js/script.js
@@ -6,4 +6,17 @@ let segundos
 var retorno = new Audio("./src/audio/retorno.mp3")
 var fim = new Audio("./src/audio/fim.mp3")
 
-
+function iniciar() {
+    if(acao.value == 0){
+        document.getElementById('erro_acao').innerHTML= "Informe os minutos"
+        acao.focus()
+    } else if (pausa.value == 0) {
+        document.getElementById('erro_pausa').innerHTML= "Informe a pausa"
+        acao.focus()
+    } else if (sessoes.value == 0) {
+        document.getElementById('erro_sessoes').innerHTML= "Informe as sessões"
+        acao.focus()
+    } else {
+        
+    }
+}
